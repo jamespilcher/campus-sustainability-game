47a120a8b6fcb4bd1d567bb4b5929ad667c75686
Ethan Hofton <ethanhofton@gmail.com>
2023-02-19 17:53:11 +0000

    fixed line length and removed unused imports

--- streatham_go/streatham_go/settings.py
+++ streatham_go/streatham_go/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g30hr$0as_p+39eb^lywk!dx6c6ya)c$($%g%1^0-dn#eq8)z0"
+SECRET_KEY = "django-insecure-g30hr$0as_p+"\
+             "39eb^lywk!dx6c6ya)c$($%g%1^0-dn#eq8)z0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,16 +89,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth."
+                "password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth."
+                "password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth."
+                "password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth."
+                "password_validation.NumericPasswordValidator",
     },
 ]
 
