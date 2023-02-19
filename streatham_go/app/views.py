d00a76eb42a87bc363096421ad8661c98f89b342
Ethan Hofton <ethanhofton@gmail.com>
2023-02-19 20:29:30 +0000

    added login and register pages

--- streatham_go/app/views.py
+++ streatham_go/app/views.py
@@ -1,3 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
 
 # Create your views here.
+def register(request):
+    return render(request, 'app/register.html')
+
+
+def login(request):
+    return render(request, 'app/login.html')
+
