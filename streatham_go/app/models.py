66dc0aaa6ccec47b066bbc8b45b9ccb15d21aa98
Daniel Moulton <dan.moulton@btinternet.com>
2023-02-24 15:59:45 +0000

    Created Leaderboard Model

--- streatham_go/app/models.py
+++ streatham_go/app/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Create a leaderboard model containing the user, their level, and their quiz count
+class Leaderboard(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.IntegerField(default=1)
+    quiz_count = models.IntegerField(default=0)
