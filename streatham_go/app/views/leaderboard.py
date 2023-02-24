22d3078c049b7d3db0f48ba740ed990870c126d6
Daniel Moulton <dan.moulton@btinternet.com>
2023-02-24 16:01:00 +0000

    Leaderboard data and context

--- streatham_go/app/views/leaderboard.py
+++ streatham_go/app/views/leaderboard.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from ..models import Leaderboard
 
 
 @login_required
 def leaderboard(request):
-    return render(request, 'app/leaderboard.html')
+    leaderboard_data = Leaderboard.objects.order_by('-level', '-quiz_count')
+    context = {'globalLeaderboard': leaderboard_data}
+    return render(request, 'app/leaderboard.html', context)
