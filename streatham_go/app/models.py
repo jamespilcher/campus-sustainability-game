e15e285ea2909f82af52ff5d23ce02f22efb9ed7
Daniel Moulton <dan.moulton@btinternet.com>
2023-02-26 19:20:51 +0000

    Moved methods from leaderboard to models

--- streatham_go/app/models.py
+++ streatham_go/app/models.py
@@ -8,3 +8,27 @@ class Leaderboard(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     level = models.IntegerField(default=1)
     quiz_count = models.IntegerField(default=0)
+
+    @classmethod
+    def get_current_user_rank(user, user_data, current_user_data):
+        return user_data.index(
+            {
+                'username': user.username,
+                'level': current_user_data.level,
+                'quiz_count': current_user_data.quiz_count
+            }) + 1
+
+    @classmethod
+    def get_current_user_data(user, leaderboard_data):
+        return leaderboard_data.filter(user=user).first()
+
+    @classmethod
+    def get_user_data(leaderboard_data):
+        user_data = []
+        for user_leaderboard_data in leaderboard_data:
+            user_data.append({
+                'username': user_leaderboard_data.user.username,
+                'level': user_leaderboard_data.level,
+                'quiz_count': user_leaderboard_data.quiz_count
+            })
+        return user_data
--- streatham_go/app/views/leaderboard.py
+++ streatham_go/app/views/leaderboard.py
@@ -3,39 +3,16 @@ from django.contrib.auth.decorators import login_required
 from ..models import Leaderboard
 
 
-def get_current_user_rank(user, user_data, current_user_data):
-    return user_data.index(
-        {
-            'username': user.username,
-            'level': current_user_data.level,
-            'quiz_count': current_user_data.quiz_count
-        }) + 1
-
-
-def get_current_user_data(user, leaderboard_data):
-    return leaderboard_data.filter(user=user).first()
-
-
-def get_user_data(leaderboard_data):
-    user_data = []
-    for user_leaderboard_data in leaderboard_data:
-        user_data.append({
-            'username': user_leaderboard_data.user.username,
-            'level': user_leaderboard_data.level,
-            'quiz_count': user_leaderboard_data.quiz_count
-        })
-    return user_data
-
-
 @login_required
 def leaderboard(request):
     leaderboard_data = Leaderboard.objects.order_by('-level', 'quiz_count')
-    user_data = get_user_data(leaderboard_data)
+    user_data = Leaderboard.get_user_data(leaderboard_data)
     user_data = sorted(user_data, key=lambda x: x['level'], reverse=True)
 
     # Find the current user's leaderboard data and rank
-    current_user_data = get_current_user_data(request.user, leaderboard_data)
-    current_user_rank = get_current_user_rank(
+    current_user_data = Leaderboard.get_current_user_data(
+        request.user, leaderboard_data)
+    current_user_rank = Leaderboard.get_current_user_rank(
         request.user, user_data, current_user_data)
 
     context = {
