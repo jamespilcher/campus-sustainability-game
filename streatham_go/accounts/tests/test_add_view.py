79e72ef82dfe0079908b16a66124780395f85003
ethan hofton <ethanhofton@gmail.com>
2023-03-01 22:28:03 +0000

    Filled out add view tests

--- streatham_go/accounts/tests/test_add_view.py
+++ streatham_go/accounts/tests/test_add_view.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.test import TestCase
 from urllib.parse import urlencode
+from ..models import Friend
+from django.db.models import Q
 
 pytest.USER_PASSWORD = '12345'
 pytest.USER_WRONG_PASSWORD = 'wrong_password'
@@ -10,35 +12,77 @@ pytest.USER_WRONG_PASSWORD = 'wrong_password'
 
 @pytest.fixture
 @pytest.mark.django_db
-def user() -> User:
-    u = User.objects.create_user('ethanhofton',
-                                 'eh736@exeter.ac.uk',
+def user1() -> User:
+    u = User.objects.create_user('ethanone',
+                                 'ethanone@exeter.ac.uk',
                                  pytest.USER_PASSWORD)
-    u.first_name = 'Ethan'
-    u.last_name = 'Hofton'
+    u.first_name = 'ethan'
+    u.last_name = 'one'
+    u.save()
+
+    return u
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def user2() -> User:
+    u = User.objects.create_user('ethantwo',
+                                 'ethantwo@exeter.ac.uk',
+                                 pytest.USER_PASSWORD)
+    u.first_name = 'ethan'
+    u.last_name = 'two'
     u.save()
 
     return u
 
 
 @pytest.mark.django_db
-def test_add_view(user, client):
-    pass
+def test_add_view(user1, user2, client):
+    client.login(username=user1.username, password=pytest.USER_PASSWORD)
+    url = reverse('accounts:add', kwargs={'username': user2.username})
+    responce = client.get(url, follow=True)
+
+    assert responce.status_code == 200
+    assert (Friend.objects.filter(user1=user1,
+                                  user2=user2).exists() or
+            Friend.objects.filter(user1=user2,
+                                  user2=user1).exists())
 
 
 @pytest.mark.django_db
-def test_add_view_sef(user, client):
-    pass
+def test_add_view_self(user1, client):
+    client.login(username=user1.username, password=pytest.USER_PASSWORD)
+    url = reverse('accounts:add', kwargs={'username': user1.username})
+    responce = client.get(url, follow=True)
+
+    assert responce.status_code == 406
+    assert not Friend.objects.filter(user1=user1,
+                                     user2=user1).exists()
 
 
 @pytest.mark.django_db
-def test_add_view_non_existant(user, client):
-    pass
+def test_add_view_non_existant(user1, user2, client):
+    client.login(username=user1.username, password=pytest.USER_PASSWORD)
+    url = reverse('accounts:add', kwargs={'username': user2.username + 'not'})
+    responce = client.get(url, follow=True)
+
+    assert responce.status_code == 404
 
 
 @pytest.mark.django_db
-def test_add_view_allready_friends(user, client):
-    pass
+def test_add_view_allready_friends(user1, user2, client):
+    f = Friend(user1=user1, user2=user2)
+    f.save()
+
+    client.login(username=user1.username, password=pytest.USER_PASSWORD)
+    url = reverse('accounts:add', kwargs={'username': user2.username})
+    responce = client.get(url, follow=True)
+
+    assert responce.status_code == 406
+    assert not (Friend.objects.filter(user1=user1,
+                                      user2=user2).exists() and
+                Friend.objects.filter(user1=user2,
+                                      user2=user1).exists())
 
 
 def test_add_view_unauthenticated(client):
