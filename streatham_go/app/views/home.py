93adbda60f146a707ea5845917c2f10de58317e4
jamespilcher <pilcherjames0@gmail.com>
2023-03-10 15:22:08 +0000

    first draft now displays all characters in place

--- streatham_go/app/views/home.py
+++ streatham_go/app/views/home.py
@@ -20,31 +20,15 @@ from django.contrib import messages
 #     return random_question
 
 
-def _get_buildings():
-    # Select todays random location
-    location_objects = list(Location.objects.filter())
-    locations = []
-
-    for location in location_objects:
-        locations.append({
-            'pk': location.pk,
-            'name': location.name,
-            'latitude': location.latitude,
-            'longitude': location.longitude,
-            'icon': location.icon
-        })
-    return locations
-
-
-@login_required
+# @login_required
 def home(request):
     context = {}
     if Location.objects.count() == 0:
         messages.error(request, ("Found no locations in database. "
                                  "Please add some locations in the "
                                  "admin panel."))
-    if not Location.objects.count():
-        locations = _get_buildings()
+    else:
+        locations = Location.objects.all()
         context = {
             'locations': locations,
             'GOOGLE_API_KEY': settings.GOOGLE_API_KEY
