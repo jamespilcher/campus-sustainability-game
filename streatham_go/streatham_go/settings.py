e748e1e30ad8438fce1ced37b6f3db9b516929cb
jamespilcher <pilcherjames0@gmail.com>
2023-03-10 12:26:12 +0000

    added media root to setings.py

--- streatham_go/streatham_go/settings.py
+++ streatham_go/streatham_go/settings.py
@@ -47,6 +47,8 @@ ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+
 # Application definition
 
 INSTALLED_APPS = [
