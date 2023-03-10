94743c65fea0abe16cbeea72aabf22689ebba71e
jamespilcher <pilcherjames0@gmail.com>
2023-03-10 13:13:44 +0000

    added comments

--- streatham_go/streatham_go/urls.py
+++ streatham_go/streatham_go/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
+# Let Django serve media files in debug mode
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
