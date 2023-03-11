e9478ac310af7f9615dc79da7ecd25acefb2a892
jamespilcher <pilcherjames0@gmail.com>
2023-03-11 18:08:57 +0000

    map now pans to the character

--- streatham_go/app/static/app/conversation.js
+++ streatham_go/app/static/app/conversation.js
@@ -1,70 +1,73 @@
 
+buildingName = ""
+buildingLatitude = ""
+buildingLongitude = ""
+buildingMessage = ""
+buildingIcon = ""
+
+function buildingSetter(building){
+  buildingName = building.name
+  buildingLatitude = Number(building.latitude)
+  buildingLongitude = Number(building.longitude)
+  buildingMessage = building.message
+  buildingIcon = building.icon
+}
+
 
 function say(msg) {
     document.getElementById('dialogue').innerHTML = msg;
   }
 
+function updateIcon(icon){
+  if (icon) {
+    icon = '<img src=/media/' + icon + ' alt="' +'"class="img-fluid mx-auto" alt="icon" style="width:100%;image-rendering:pixelated">';
+  }
+  document.getElementById('icon').innerHTML = icon
+}
+
 function updateButtons(responses) {
     document.getElementById('buttons').innerHTML = responses;
 }
 
-function welcomeScene(welcomeMessage){
-    say(welcomeMessage)
+function emptyScene(){
+  say("")
+  updateButtons("")
+  updateIcon("")
+}
+
+function welcomeScene(){
+    say(buildingMessage)
     responses = "<button type='button' class='btn btn-danger' onclick='say(\"Insult\")'>Insult</button>" +
     "<button type='button' class='btn btn-success' onclick='getLocation()'>I'm here</button>" +
     "<button type='button' class='btn btn-warning' onclick='say(\"Open in Maps\")'>Open in Maps</button>"
-
+    updateIcon(buildingIcon)
     updateButtons(responses)
 }
 
+function okayThenScene(){
+  say("Very well.")
+  responses = "<button type='button' class='btn btn-danger' onclick='emptyScene()'>Explore</button>"
+  updateButtons(responses)
+}
 
 function readyScene(){
-    say("Are you ready to start?")
-    responses = "<button type='button' class='btn btn-primary' onclick='questionScene()'>Yes</button>" +
-    "<button type='button' class='btn btn-primary' onclick='welcomeScene()'>No</button>"
+    say("Ah! Good to see you! Are you ready to play?")
+    responses = "<button type='button' class='btn btn-primary' onclick='redirect()'>Yes</button>" +
+    "<button type='button' class='btn btn-primary' onclick='okayThenScene()'>No</button>"
     updateButtons(responses)
 }
-
-function questionScene(){
-    message = question + " <br> <br>" +
-    "A) " + a + "<br>" +
-    "B) " + b + "<br>" +
-    "C) " + c + "<br>" +
-    "D) " + d
-    say(message)
-    //TODO here:
-    responses = "<button type='button' class='btn btn-warning' onclick='checkAnswer(\"a\")'>A</button>" +
-    "<button type='button' class='btn btn-primary' onclick='checkAnswer(\"b\")'>B</button>" +
-    "<button type='button' class='btn btn-danger' onclick='checkAnswer(\"c\")'>C</button>" +
-    "<button type='button' class='btn btn-success' onclick='checkAnswer(\"d\")'>D</button>"
-
-    updateButtons(responses)
-}
-
-function quizCompletedScene(){
-  //TODO how many points have they won...?
-  say("You have completed today's question... I won't meet with you again today...")
-  updateButtons("")
-}
-
-function correctAnswerScene(){
-  //TODO how many points have they won...?
-  say("That's correct. You have successfully earned points! Safe travels!")
-  responses = "<button type='button' class='btn btn-primary' onclick='quizCompletedScene()'>Thank you!</button>"
-  updateButtons(responses)
-}
-
-function incorrectAnswerScene(){
-  say("Sorry, that's incorrect. Better luck tomorrow... Safe travels!")
-  responses = "<button type='button' class='btn btn-primary' onclick='quizCompletedScene()'>Okay</button>"
-  updateButtons(responses)
-}
-
 
 function unsuccessfulLocationCheckScene(){
-  say("Sorry, I can't seem to see that you are here...")
-  responses = "<button type='button' class='btn btn-primary' onclick='welcomeScene()'>Okay</button>" +
+  say("Sorry, I can't seem to verify that you are here...")
+  responses = "<button type='button' class='btn btn-Danger' onclick='emptyScene()'>Explore</button>"
   updateButtons(responses)
+}
+
+
+function redirect(){
+  say("redirecting...")
+  updateButtons("")
+  updateIcon("")
 }
 
 // location checking
@@ -74,9 +77,7 @@ function error(err){
     unsuccessfulLocationCheckScene()
   }
 function success(pos){
-
-  crds = pos.coords.longitude + "," + pos.coords.latitude
-  if (isValidLocation(crds)){
+  if (isValidLocation(pos.coords.latitude, pos.coords.longitude)){
     readyScene();
   }
   else {
@@ -93,32 +94,8 @@ function getLocation(){
   navigator.geolocation.getCurrentPosition(success, error, options);
 }
 
-
-// TODO: implement this function. To return a bool of 'yes' valid, or 'no' invalid
-// Called from success() in getLocation()
-function isValidLocation(crds){
-  return true;
+// checks if within 50m of building (more or less).
+function isValidLocation(userLatitude, userLongitude){
+  userDist = Math.sqrt((userLatitude - buildingLatitude)**2 + (userLongitude - buildingLongitude)**2)
+  return userDist <= 0.0008;
 }
-
-// answer checking:
-
-function checkAnswer(answer){
-  if (processAnswer(answer)){
-    correctAnswerScene();
-  }
-  else {
-    incorrectAnswerScene();
-  }
-}
-
-// TODO: implement this function. To return a bool of 'yes' correct, or 'no' incorrect.
-// Backend must: write how mny points they have won to the users profile
-function processAnswer(answer){
-  return answer == "a"; // for temporary testing
-}
-
-
-
-// Bonus, not needed for prototype:
-// Additionally, at the moment there is no checks to see if the user has already answered the question, or if they have already been to the location. This is something that needs to be implemented in the backend.
-// also need to track how long user takes to answer
