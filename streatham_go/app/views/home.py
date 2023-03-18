088a54a107eda502bdfb0640761d968021db7274
jamespilcher <pilcherjames0@gmail.com>
2023-03-18 19:01:19 +0000

    removed old code

--- streatham_go/app/views/home.py
+++ streatham_go/app/views/home.py
@@ -7,18 +7,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core import serializers
 
-# def _generate_building_question():
-#     # Seed random number generator with date
-#     a = datetime.datetime.now() + datetime.timedelta(days=1)
-#     b = a.strftime("%Y%m%d" + settings.SECRET_KEY)  # add django seed
-#     random.seed(b)
-
-#     # Select todays random question
-#     pks = Question.objects.values_list('pk', flat=True)
-#     random_pk = random.choice(pks)
-#     random_question = Question.objects.get(pk=random_pk)
-#     return random_question
-
 
 @login_required
 def home(request):
