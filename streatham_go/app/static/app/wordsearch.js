340924846ca4b8c2374eb26e7fd4df6d40c7bfd1
Alex Gulliver <alex.gulliver6@gmail.com>
2023-03-20 15:12:41 +0000

    Basic wordsearch implementation

--- streatham_go/app/static/app/wordsearch.js
+++ streatham_go/app/static/app/wordsearch.js
@@ -1,3 +1,10 @@
+// Global variable declarations
+var gameOver = false;
+const numRows = 11;
+const numCols = 11;
+const table = createTable(numRows, numCols);
+//var score = 0;
+
 // Function to create the wordsearch grid table
 function createTable(rows, cols) {
   const table = document.createElement('table');
@@ -16,11 +23,12 @@ function createTable(rows, cols) {
 function fillEmptyCells(table) {
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const rows = table.rows;
+  // Adds all empty cells to array
   const emptyCells = [];
   for (let i = 0; i < rows.length; i++) {
     const cells = rows[i].cells;
     for (let j = 0; j < cells.length; j++) {
-      // Adds all empty cells to array
+      // Check if cell is empty
       if (cells[j].textContent === '') {
         emptyCells.push(cells[j]);
       }
@@ -42,7 +50,7 @@ function insertWords(words, table) {
     let x, y, dx, dy;
     do {
       // Random starting position and direction
-      x = Math.floor(Math.random() * numCols);
+      x = Math.floor(Math.random() * numCols); 
       y = Math.floor(Math.random() * numRows);
       const direction = Math.floor(Math.random() * 2); // 2 possible directions
       dx = direction === 0 ? 1 : 0; // Horizontal direction
@@ -77,6 +85,7 @@ function insertWords(words, table) {
         const cell = table.rows[y + dy * i].cells[x + dx * i];
         cell.textContent = word.charAt(i);
       }
+      // Adds word coordinates (for start and end character) to dictionary
       wordCoords.push({
         word: word,
         startX: x,
@@ -89,56 +98,37 @@ function insertWords(words, table) {
   }
 }
 
+let clickedCells = [];
 
-// Function to let users select a word in the table
-// function selectWord(table, words) {
-//   let startCell = null;
-//   let endCell = null;
-//   let wordCells = null;
+// Function allowing to click cells and also select words
+// Works by letting you click first and last character of a word 
+// If it matches the word then it will change the background color of the cells and increase the score
 
-// let validEndSelection = true;
-// while (validEndSelection == true) { 
-//   table.querySelectorAll('td').forEach(cell => {
-//     cell.addEventListener('click', () => {
-//       if (!startCell) {
-//         startCell = cell;
-//         startCell.style.backgroundColor = 'yellow';
-//         console.log(`Start cell: row ${startCell.parentNode.rowIndex}, col ${startCell.cellIndex}`);
-//       } else {
-//         endCell = cell;
-//         endCell.style.backgroundColor = 'yellow';
-//         if (endCell.parentNode.rowIndex == startCell.parentNode.rowIndex) {
-//           const startColIndex = startCell.cellIndex;
-//           const endColIndex = endCell.cellIndex;
-//           const row = startCell.parentNode;
-//           const word = [];
-//           for (let i = startColIndex; i <= endColIndex; i++) {
-//             const cell = row.cells[i];
-//             word.push(cell.textContent);
-//           }
-//           console.log(word.join(''));
-//         }
-//         if (startCell.cellIndex == endCell.cellIndex) {
-//           // Do nothing
-//         } else {
-//           validEndSelection = false;
-//         }
-//       }
-//     });
-//   });
-//   }
-// }
-
-function addClickListeners() {
+function addClickListeners(words) {
+  let wordsFound = 0;
+  var score = 0;
+  scoreDisplayBox(score);
+  displayWordList(words);
+  console.log("WORD LIST: " + words);
+  console.log('Score: ' + score);
   for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
       table.rows[i].cells[j].addEventListener('click', function() {
+
+        // For testing purposes
+        // console.log("WORDLIST LENGTH: " + words.length);
+
+        // Max score is 6
+        // Corresponding to number of words in the word list
+        if (score >= 6) {
+          return;
+        }
         if (clickedCells.length === 0) {
           clickedCells.push({
             row: i,
             col: j,
           });
-          table.rows[i].cells[j].style.backgroundColor = '#fff';
+          table.rows[i].cells[j].style.backgroundColor = 'yellow';
         } else {
           clickedCells.push({
             row: i,
@@ -154,7 +144,6 @@ function addClickListeners() {
                 for (let j = word.startX; j <= word.endX; j++) {
                   table.rows[word.startY].cells[j].style.backgroundColor = '#0f0';
                   table.rows[word.startY].cells[j].style.pointerEvents = 'none';
-
                 }
               }
               // If word is vertical, loop through the cells and change the background color
@@ -162,9 +151,22 @@ function addClickListeners() {
                 for (let j = word.startY; j <= word.endY; j++) {
                   table.rows[j].cells[word.startX].style.backgroundColor = '#0f0';
                   table.rows[j].cells[word.startX].style.pointerEvents = 'none';
-
                 }
               }
+              // Word found condition starts here
+              console.log('WORD FOUND: ' + word.word);
+              score++;
+              console.log('Score: ' + score); // Displays word to console once it has been found (testing purposes)
+              scoreDisplayBox(score);
+              // loop that goes through words and removes word.word from it
+              for (let i = 0; i < words.length; i++) {
+                if (words[i] === word.word) {
+                  words.splice(i, 1);
+                  i = words.length;
+                }
+              }
+              displayWordList(words);
+              console.log('HELLO')
               wordCoords.splice(i, 1); // remove word from wordCoords
               i = wordCoords.length;
             } else {
@@ -178,24 +180,113 @@ function addClickListeners() {
   }
 }
 
-let clickedCells = [];
+let previousWordList = null;
 
-const numRows = 11;
-const numCols = 11;
-const table = createTable(numRows, numCols);
-// List of words to place in grid (sustainable)
-const words = ['OOOOOP', 'PLLLLL', 'SOLAR', 'DIYYY', 'CARBON'];
+function displayWordList(words) {
+
+  // Deletes previous word list
+  if (previousWordList) {
+    document.body.removeChild(previousWordList);
+  }
+
+  const wordList = document.createElement('div');
+  wordList.style.margin = '0 auto';
+  wordList.style.marginTop = '10px';
+  wordList.style.width = 'fit-content';
+
+  for (let i = 0; i < words.length; i++) {
+    const word = document.createElement('div');
+    word.textContent = words[i];
+    word.style.display = 'inline-block';
+    word.style.marginRight = '10px';
+    word.style.fontSize = '20px';
+    word.style.fontWeight = 'bold';
+    word.style.color = 'black';
+    wordList.appendChild(word);
+  }
+  document.body.appendChild(wordList);
+  previousWordList = wordList;
+  return wordList;
+}
+
+
+// Random word selection prototype
+
+// const wordbank = ['REUSE', 'RECYCLE', 'COMPOST', 'ENERGY', 'ECOLOGY', 'SOLAR', 'WIND', 'BIOFUEL', 'CARPOOL', 'CONSERVE', 'CONSUME', 'DROUGHT', 'ECOSYSTEM', 'FOSSILFUEL', 'GARDENING', 'GREENBELT', 'GREENHOUSE', 'HABITAT', 'INSULATION', 'LANDFILL', 'NATURAL', 'ORGANIC', 'POLLUTION', 'PRECIPITATION', 'RENEWABLE', 'RESOURCES', 'SUSTAIN', 'WATER', 'WATERSHED', 'BIODIVERSITY', 'CLIMATE', 'COAL', 'CONSERVATION', 'DEFOREST', 'DEGRADATION', 'DEPLETION', 'ELECTRIC', 'ENVIRONMENT', 'ETHICAL', 'FERTILIZER', 'GREEN', 'HARVEST', 'HEAT', 'HYDROPOWER', 'LANDSCAPE', 'NATURERESERVE', 'OCEAN', 'REDUCE', 'ETHICAL', 'REFOREST', 'REGULATE', 'SOIL', 'TRANSPORT', 'UPCYCLE', 'VEGANISM', 'WASTE', 'WILDLIFE', 'CARBON', 'ECOFRIENDLY', 'EFFICIENCY', 'EQUITY', 'FOODMILES', 'FRUGAL', 'GEOTHERMAL', 'GREENSPACE', 'HABITABLE', 'INSECTARY', 'LOWCARBON', 'METHANE', 'NATURE', 'OZONE', 'PERMACULTURE', 'PLANTING', 'RAINWATER', 'REFUSE', 'RESOURCEFUL', 'SHADETREE', 'SUSTAINABLE'];
+
+// // Function to select 6 random words from words array
+// function selectRandomWords(words) {
+//   let randomWords = [];
+//   for (let i = 0; i < 6; i++) {
+//     let randomIndex = Math.floor(Math.random() * words.length);
+//     randomWords.push(words[randomIndex]);
+//     words.splice(randomIndex, 1);
+//   }
+//   return randomWords;
+// }
+
+// const words = selectRandomWords(wordbank); 
 
 let wordCoords = [];
+let scoreDisplay = null;
+
+function scoreDisplayBox(score) {
+  if (scoreDisplay) {
+    document.body.removeChild(scoreDisplay);
+  }
+
+  const scoreToDisplay = score;
+  scoreDisplay = document.createElement('div');
+  scoreDisplay.textContent = ('Score: ' + scoreToDisplay);
+  scoreDisplay.style.margin = '0 auto';
+  scoreDisplay.style.marginTop = '10px';
+  scoreDisplay.style.width = 'fit-content';
+  scoreDisplay.style.fontSize = '20px';
+  scoreDisplay.style.fontWeight = 'bold';
+  scoreDisplay.style.color = 'black';
+  document.body.appendChild(scoreDisplay);
+}
+
+
+// Timer prototype
+// let timeLeft = 60;
+// const timer = document.createElement('div');
+// timer.style.margin = '0 auto';
+// timer.style.marginTop = '25px';
+// timer.style.width = 'fit-content';
+// timer.style.fontSize = '20px';
+// timer.style.fontWeight = 'bold';
+// timer.style.color = 'black';
+// document.body.appendChild(timer);
+// const timerInterval = setInterval(() => {
+//   if (timeLeft > 0) {
+//     timer.textContent = `Time left: ${timeLeft}`;
+//     timeLeft--;
+//   } else {
+//     timer.textContent = 'Time is up!';
+//     clearInterval(timerInterval);
+//   }
+// }, 1000);
+
+
+function runGame(table, words, wordCoords) {
+    fillEmptyCells(table);
+    insertWords(words, table);
+    console.log(wordCoords);
+    //selectWord(table, words);
+    //displayWordList(words);
+    document.body.appendChild(table);
+    addClickListeners(words);
+}
 
 // Styling
 table.style.margin = '0 auto';
 table.style.border = '1px solid black';
-table.style.marginTop = '25px';
+table.style.marginTop = '10px';
 
-fillEmptyCells(table);
-insertWords(words, table);
-console.log(wordCoords);
-//selectWord(table, words);
-document.body.appendChild(table);
-addClickListeners();
+// List of words to place in grid (sustainable)
+
+const words = ['REUSE', 'RECYCLE', 'COMPOST', 'ENERGY', 'ECOLOGY', 'SOLAR']
+
+// Game run function
+runGame(table, words, wordCoords);
