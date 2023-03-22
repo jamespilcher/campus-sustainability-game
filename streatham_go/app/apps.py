17674d41a740bf2bcdda77f90fc185c6081eba36
Daniel Moulton <dan.moulton@btinternet.com>
2023-03-22 15:57:31 +0000

    Revert "Reset timesPlayedToday functionality"
    This reverts commit 4034fb398d8190ccb3d6f9407f8932a288f0d74f.

--- streatham_go/app/apps.py
+++ streatham_go/app/apps.py
@@ -1,15 +1,6 @@
 from django.apps import AppConfig
-from django.utils import timezone
-from schedule import Scheduler
-from models import reset_times_played_today
 
 
-class YourAppConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
-
-    def ready(self):
-        scheduler = Scheduler()
-        scheduler.timezone = timezone.get_current_timezone()
-        scheduler.every().day.at('00:00').do(reset_times_played_today)
-        scheduler.start()
