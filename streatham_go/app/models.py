761317ccb2154c44306a7310ad03010da219e949
Daniel Moulton <dan.moulton@btinternet.com>
2023-03-22 23:23:50 +0000

    Revert "resetTimesPlayed method"
    This reverts commit ce1ab7f2eb10fed820bad8b7c8d252e5ccb51043.

--- streatham_go/app/models.py
+++ streatham_go/app/models.py
@@ -88,7 +88,3 @@ class Leaderboard(models.Model):
                 'numGamesPlayed': user_leaderboard_data.numGamesPlayed
             })
         return user_data
-
-
-def reset_times_played_today():
-    Leaderboard.objects.update(timesPlayedToday=0)
