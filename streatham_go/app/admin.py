0d8ffc10b8ab541968151f6f1aa20e9e3fe2a2a4
jamespilcher <pilcherjames0@gmail.com>
2023-03-23 01:43:27 +0000

    added word to admin.py

--- streatham_go/app/admin.py
+++ streatham_go/app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from app.models import Location, Leaderboard, Game
+from app.models import Location, Leaderboard, Game, Word
 # Register your models here.
 
 # register all the models to the admin page
 admin.site.register(Leaderboard)
 admin.site.register(Location)
 admin.site.register(Game)
+admin.site.register(Word)
